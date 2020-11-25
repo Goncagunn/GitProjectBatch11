8c7a5a0f18d1ecd3cdbb2fec004e90d95b1f0b75
unknown <heratan2501@gmail.com>
2020-11-25 15:04:23 +0300

    ikikci gun 2.commit

--- src/git/BirinciGun.java
+++ src/git/BirinciGun.java
@@ -6,5 +6,7 @@ public class BirinciGun {
 	   System.out.println("11:05 -Birinci Gun");
 	   System.out.println("Ikici gun");
 	   System.out.println("10:11 -ikinci gun");
+	   System.out.println("10:26 -ikinci gun");
+	   
 }
 }
